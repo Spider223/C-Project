fe95bebab56386c4e67b78e80df5eb70ae6f3e02
madhan <madhanpokhrel9@gmail.com>
2022-06-21 22:01:34 +0545

    updated

---
+++ quizgame.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout<<"\t\t\t========================\n";
+    cout<<"\t\t\t**** Welcome to Quiz Game ****\n";
+    cout<<"\t\t\t========================\n";
+    cout<<"***Developed by Madhan Pokharel***\n";
+    cout<<"===============================\n";
+
+    cout<<
+
+}
